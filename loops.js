5a835d5bc9020cb006ba7f66954d10ad91d024af
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-15 21:58:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,11 +17,13 @@ function whileLoop (n){
   }
   return "done"
 }
-function maybeTrue(){
-  return Math.random() >= 0.5
-}
+
 
 function doWhileLoop(array){
+  function maybeTrue(){
+  return Math.random() >= 0.5
+  }
+  
   do{
     array.unshift()
   } while (maybeTrue() && array.length > 0)
